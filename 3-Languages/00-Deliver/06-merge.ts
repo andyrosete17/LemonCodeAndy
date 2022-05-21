3752999de62741d9e34c16ca83ec76a375067894
andy <andyrosete17@gmail.com>
2022-05-21 11:20:58 +0200

    First approach of the languages deliverables code

---
+++ 3-Languages/00-Deliver/06-merge.ts
@@ -0,0 +1,8 @@
+const merge = (primaryArray, secondaryArray) => [
+    ...primaryArray,
+    ...secondaryArray,
+  ];
+  
+  const a = { name: "Maria", surname: "Ibañez", country: "SPA" };
+  const b = { name: "Luisa", age: 31, married: true };
+
