9921dabdfc8fbcdc3a6a05123926d13f48dbf872
andy <andy.rosete@novafori.com>
2022-06-19 19:26:49 +0200

    update code to work with envs

--- 4-Bundle/task-2/config.js
+++
@@ -1,6 +0,0 @@
-const result = require("dotenv").config({
-  path: "./.env",
-});
-
-console.log("andy aqui entramos", result);
-export const { API_KEY } = result.parsed;
--- 4-Bundle/task-2/src/presentation/index.tsx
+++ 4-Bundle/task-2/src/presentation/index.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import classes from "./styles.scss";
 
-// let logos = require("../content/logo_2.png");
 import logo from "../content/logo_2.png";
 
-import { API_KEY } from "../../config";
-
 export const PresentationComponent = () => {
-  console.log("primero vemos el env", API_KEY);
+  console.log("The API key for the env selected is", process.env.API_KEY);
 
-  console.log(`process.env`, process.env);
+  console.log(`process.env`, process.env.NODE_ENV);
   return (
     <div>
-      {/* <h1>aqui dice {process.env.REACT_APP_API_KEY}</h1> */}
-      <h1 className={classes.colorFont}>Hola Mundo LemonCode</h1>
+      <h1>aqui dice {process.env.API_KEY}</h1>
+      <h1 className={classes.colorFont}>Hello World LemonCode</h1>
       <img src={logo} alt="logoImage" />
     </div>
   );
